243793255f8a2a39e7bc207fbcaae3b82df1e06e
Szczepan Smolen <szczepan.smolen@siemens.com>
2025-12-26 10:59:58 +0100

    bug fixing

--- custom_components/tge_rdn/config_flow.py
+++ custom_components/tge_rdn/config_flow.py
@@ -44,13 +44,16 @@ class TGERDNConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return TGERDNOptionsFlow(config_entry)
 
 class TGERDNOptionsFlow(config_entries.OptionsFlow):
+    """Handle options flow for TGE RDN."""
+
     def __init__(self, config_entry):
-        self.config_entry = config_entry
+        """Initialize options flow."""
+        self._config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
-        opts = self.config_entry.options
+        opts = self._config_entry.options
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
